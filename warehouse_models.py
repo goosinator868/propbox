b884c50a925239b3a7e68d47e1a2a6684ebd8e39
William Rosser <will.e179@gmail.com>
2017-08-28 20:03:58 -0400

    Update to Item model
    Items now have four new fields: older_version, newer_version, deleted, and outdated. Full description is in the comments of warehouse_models.py
    
    The display now properly shows only items that are not deleted and not outdated. This logic happens in templates/index.html.

--- warehouse_models.py
+++ warehouse_models.py
@@ -7,3 +7,10 @@ class Item(ndb.Model):
     name = ndb.StringProperty(required=True)
     description = ndb.StringProperty(required=False)
     qr_code = ndb.IntegerProperty(required=False)
+    #older_version and newer_version hold keys to other versions of the item in the database.
+    older_version = ndb.KeyProperty(required=False)
+    newer_version = ndb.KeyProperty(required=False)
+    #deleted is True if an item was deleted by a user but has not yet been purged from the system.
+    deleted = ndb.BooleanProperty(required=True,default=False)
+    #outdated is True if there is a newer version to be shown; items with outdated=True will not be displayed.
+    outdated = ndb.BooleanProperty(required=True,default=True)
