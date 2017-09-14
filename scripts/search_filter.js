e37c295a601765114d7d7c8dbd61720ec9098dc5
syoung <sydneyyoung17@gmail.com>
2017-09-14 18:15:58 -0400

    Checkbox persistence for condition fixed

--- scripts/search_filter.js
+++
@@ -1,7 +0,0 @@
-function displayFilterCostumeForm(x) {
-    document.getElementById("costume_specific_forms").style.display = 'block'
-}
-
-function hideFilterCostumeForm(x) {
-    document.getElementById("costume_specific_forms").style.display = 'none'
-}
