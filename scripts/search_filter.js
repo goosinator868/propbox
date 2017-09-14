cc30ffd9a0508246d1d83b13e0f8f92f4e933aaf
syoung <sydneyyoung17@gmail.com>
2017-09-14 10:48:47 -0400

    removed js file for filter page since it's not working yet

---
+++ scripts/search_filter.js
@@ -0,0 +1,7 @@
+function displayFilterCostumeForm(x) {
+    document.getElementById("costume_specific_forms").style.display = 'block'
+}
+
+function hideFilterCostumeForm(x) {
+    document.getElementById("costume_specific_forms").style.display = 'none'
+}
