cd7ebe92bc3a3005497f89261478bcd5e729719d
Henry Peteet <hvpeteet@gmail.com>
2017-09-26 23:49:49 -0400

    Refactored webdriver test
    Optimized for use in future tests.

--- tests/login_test.py
+++ tests/login_test.py
@@ -10,16 +10,19 @@ from selenium import webdriver
 #     1) I placed it in C:\ProgramFiles\chromedriver
 #     2) I added C:\ProgramFiles\chromedriver to my path
 
-
-def main():
+def setupTestingEnv():
     # Setup
     driver = webdriver.Chrome()
     driver.get("localhost:8080")
     # Tell the browser to wait for up to 10s before reporting that it can't find an element.
     driver.implicitly_wait(10)
+    return driver
+
+
+def login(driver, email="test@fake.org", name="Automated Test", password="password"):
     # First screen
-    email = driver.find_element_by_name('email')
-    email.send_keys("test@fake.org")
+    email_elem = driver.find_element_by_name('email')
+    email_elem.send_keys(email)
     next = driver.find_element_by_css_selector('.firebaseui-id-submit')
     next.click()
     # Second screen
@@ -28,21 +31,21 @@ def main():
     #    2) The automated tester has made an account and just needs to log in.
     try:
         # The automated tester has been run before.
-        password = driver.find_element_by_name('password')
-        password.send_keys("password")
+        password_elem = driver.find_element_by_name('password')
+        password_elem.send_keys(password)
     except:
         # It is the first time the automated tester has been run.
         # This will take 10s at least since we implicitly wait 10s before throwing exceptions.
         new_password = driver.find_element_by_name('newPassword')
-        new_password.send_keys("password")
-        name = driver.find_element_by_name('name')
-        name.send_keys("Automated Test")
+        new_password.send_keys(password)
+        name_elem = driver.find_element_by_name('name')
+        name_elem.send_keys(name)
         
     next = driver.find_element_by_css_selector('.firebaseui-id-submit')
     next.click()
-    # Done
-    print("passed login test")
-    driver.quit()
 
 if __name__ == '__main__':
-    main()
+    driver = setupTestingEnv()
+    login(driver)
+    print("done")
+    driver.quit()
